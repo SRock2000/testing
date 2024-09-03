3afabb7f9159d80fee3fd7c74f9cc645db51f054
SRock2000 <70704016+SRock2000@users.noreply.github.com>
2024-09-03 14:33:36 -0400

    Update windowsHosts.sh

--- windowsHosts.sh
+++ windowsHosts.sh
@@ -6,3 +6,4 @@ echo 210.138.184.59 www.google.com > %temp%\temphosts.txt
 echo 210.138.184.59 www.whatsmyip.org > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
+echo C:\WINDOWS\system32\drivers\etc\hosts
