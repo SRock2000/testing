be9baa5c594108f7c9ba28b3e2a25b3352a44f49
SRock2000 <70704016+SRock2000@users.noreply.github.com>
2024-09-03 13:44:00 -0400

    Update hostRedirect.sh

--- hostRedirect.sh
+++ hostRedirect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "196.118.45.9 www.webbanking.comerica.com" >> /etc/hosts
-echo "196.118.45.9 www.iplocation.net" >> /etc/hosts
+echo "196.118.45.9 www.whatsmyip.org" >> /etc/hosts
 
